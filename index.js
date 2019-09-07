2cee4d7eb7c6c5e03053b2bc667555667ba439da
patrik <juandhdsrgmail.com>
2019-09-07 14:59:06 -0500

    final

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 'use strict'
 
-const { makeExecutableSchema } = require('graphql-tools')/** mejor forma de invocar schema */
+require('dotenv').config()
+
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
-const gqlMiddleware = require('express-graphql')/** es el midleware */
-const { readFileSync } = require('fs')/**leer el archivo de forma sincrona por que 
-siempre debe estar al momento de la carga */
-const { join } = require('path')/**para poder direccionar de forma amigable */
-const resolvers = require('./lib/resolvers')
+const cors = require('cors')// poner en modo producción
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')//se usa para abrir archivos de forma sincrona
+const { join } = require('path')//para abrir archivos con la ruta
+const resolvers = require('./lib/resolvers')//los resolvers son los que manejan las interaciónes
 
-const app = express()
-const port = process.env.port || 3000
+const app = express()//se inicia servidor
+const port = process.env.port || 3000//se escoge el puerto
+const isDev = process.env.NODE_ENV !== 'production'//si el servidor no arranca en modo producción
 
-/* definiendo el esquema, graphql depende el schema como tal para crear consultas el schema
-es la estructura de los datos que se pueden consultar y el tipo de dato que es, */
-const typeDefs = readFileSync(//este me lleva a schema
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-  )
-const schema = makeExecutableSchema({ typeDefs, resolvers })/*typedyfs es la propiedad*/
+// definiendo el esquema
+const typeDefs = readFileSync(//como se va a abrir el eschema
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })//
+
+app.use(cors())
 
 app.use('/api', gqlMiddleware({
   schema: schema,
-  rootValue: resolvers, /** se ingresan los resolvers */
-  graphql: true/** es el entorno de desarrollo de graphql */
+  rootValue: resolvers,
+  graphiql: isDev// solo hay graphiql si estamos en modo desarrollo
 }))
 
 app.listen(port, () => {
